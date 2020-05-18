4ac3f0a16e3c74ab03399bd2656da282a3dfefb1
Flori-87 <fcmelrod@gmail.com>
2020-05-18 07:52:53 +0200

    nuevos intentos

--- src/image_processing.py
+++ src/image_processing.py
@@ -13,3 +13,17 @@ def colorImage(series_value, path, debug):
         return cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
     return cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
 
+def sizeImage(series_value, size):
+    print("inter_cubic")
+    return cv2.resize(series_value, size, interpolation=cv2.INTER_CUBIC) 
+
+def normalizeImage(series_value):
+    return series_value/np.amax(series_value) 
+
+def imageDenoiseMEdianBlur(series_value, ksize):
+    return cv2.medianBlur(series_value, ksize)
+
+def imageDenoiseBlur(series_value, ksize):
+    return cv2.blur(series_value, ksize)
+
+
